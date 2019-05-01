aef55ad96669f19802cffc2e5aaf0263412bbddb
sifudiep <code.sifudiep@gmail.com>
2019-05-01 11:04:00 +0200

    Added more instructions

--- Program.cs
+++ Program.cs
@@ -36,10 +36,13 @@ namespace Blone
             }
             else if (input == "!help")
             {
-                Console.WriteLine("Move the hero using the WASD keys.");
-                Console.WriteLine("Control the field of view with the arrow keys.");
-                Console.WriteLine("You can also shoot using the arrow keys, however you may only shoot in the direction that your watching.");
-                Console.WriteLine("Meaning that to shoot to the left, you must first have placed you field of view to the left of your hero.");
+                Console.WriteLine("-Move the hero using the WASD keys.");
+                Console.WriteLine("-Control the field of view with the arrow keys.");
+                Console.WriteLine("-You can also shoot using the arrow keys, however you may only shoot in the direction that your watching.");
+                Console.WriteLine("-Meaning that to shoot to the left, you must first have placed you field of view to the left of your hero.");
+                Console.WriteLine("-Using the number buttons above WASD (not on the numpad) you can switch guns.");
+                Console.WriteLine("-In the bottom left corner you will also see your ammo,\n once you deplete your ammo you can either reload the guns or switch to another one using the number buttons");
+                Console.WriteLine("-Once your health reaches 0, you lose the game.");
                 Console.ReadLine();
                 goto Start;
             }
